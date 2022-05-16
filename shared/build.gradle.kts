1dfc8bb90dee73ec02ba891c99ece174323dd6ce
Nathan PRAT <nathan.prat@gmail.com>
2022-05-16 18:21:35 +0200

    WIP add dependency b/w cargoBuild and build

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -69,9 +69,25 @@ cargo {
     prebuiltToolchains = true
 }
 
+// dependsOn(tasks.named("cargoBuild")) FAILS with
+// Caused by: org.gradle.api.UnknownTaskException: Task with name 'cargoBuild' not found in project ':shared'
+// CHECK: println("tasks.names: ${tasks.names}")
+// no task with name cargo in the list...
+// probably due to the fact that this project is NOT android so "org.mozilla.rust-android-gradle.rust-android" does not add it at this point?
+task<Exec>("cargoBuildExec") {
+    // for some reason on windows we need "cmd /c" else
+    //    Execution failed for task ':shared:cargoBuildExec'.
+    //    > A problem occurred starting process 'command './gradlew''
+    // cf https://docs.gradle.org/current/dsl/org.gradle.api.tasks.Exec.html
+    commandLine("cmd", "/c", "gradlew", "--info", "cargoBuild")
+    workingDir = rootDir
+}
+
 // https://github.com/mozilla/rust-android-gradle
 tasks.whenTaskAdded {
+    // TODO? https://github.com/mozilla/rust-android-gradle/issues/85
+    //    if (name == "mergeDebugJniLibFolders" || name == "mergeReleaseJniLibFolders") {
     if (name == "javaPreCompileDebug" || name == "javaPreCompileRelease") {
-//        dependsOn(tasks.named("cargoBuild"))
+        dependsOn(tasks.named("cargoBuildExec"))
     }
 }
