6d5e1f9934eb9f3bcd688b2f74d0f829826be294
Nathan Prat <nathan.prat@gmail.com>
2022-07-21 12:05:33 +0200

    WIP substrate-client: retrieve list of pending circuits

--- shared/rust/substrate-client/src/lib.rs
+++ shared/rust/substrate-client/src/lib.rs
@@ -12,7 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use frame_support::pallet_prelude::*;
 use sp_keyring::AccountKeyring;
+use codec::{Decode, Encode};
 
 use substrate_api_client::{
     compose_extrinsic, rpc::WsRpcClient, Api, Hash, Pair, UncheckedExtrinsicV4, XtStatus,
@@ -28,16 +30,21 @@ pub mod jni_wrapper;
 
 mod loggers;
 
-// https://github.com/scs/substrate-api-client/blob/master/examples/example_generic_extrinsic.rs
-fn call_extrinsic(url: &str) -> Hash {
-    println!("[+] call_extrinsic: {:?}", url); // OK
+fn get_api(ws_url: &str) -> Api<sp_core::sr25519::Pair, WsRpcClient> {
+    println!("[+] call_extrinsic: {:?}", ws_url);
     let from = AccountKeyring::Alice.pair();
     println!("[+] call_extrinsic: from {:?}", from.public());
-    let client = WsRpcClient::new(&url); // OK
+    let client = WsRpcClient::new(&ws_url);
     println!("[+] call_extrinsic: client {:?}", client);
     let api = Api::new(client).map(|api| api.set_signer(from)).unwrap();
     println!("[+] call_extrinsic: api {:?}", api.genesis_hash.to_string());
 
+    api
+}
+
+// https://github.com/scs/substrate-api-client/blob/master/examples/example_generic_extrinsic.rs
+// TODO replace by ocw-garble garbleAndStripSigned(and update params)
+fn call_submit_config_display_signed(api: &Api<sp_core::sr25519::Pair, WsRpcClient>, is_message: bool) -> Hash {
     ////////////////////////////////////////////////////////////////////////////
     // // "set the recipient"
     // let to = AccountKeyring::Bob.to_account_id();
@@ -58,7 +65,8 @@ fn call_extrinsic(url: &str) -> Hash {
         // MUST match the name in /substrate-offchain-worker-demo/runtime/src/lib.rs
         "OcwCircuits",
         // MUST match the call in /substrate-offchain-worker-demo/pallets/ocw-circuits/src/lib.rs
-        "submit_config_display_signed" // no param
+        "submit_config_display_signed",
+        is_message
     );
 
     println!("[+] Composed Extrinsic:\n {:?}\n", xt);
@@ -72,18 +80,54 @@ fn call_extrinsic(url: &str) -> Hash {
     tx_hash.expect("send_extrinsic failed")
 }
 
+// The custom struct that is to be decoded. The user must know the structure for this to work, which can fortunately
+// be looked up from the node metadata and printed with the `example_print_metadata`.
+// MUST match pallets/ocw-garble/src/lib.rs AccountToPendingCircuitsMap
+#[derive(Encode, Decode, Debug, Clone)]
+struct StrippedCircuitPackage {
+    pgarbled_cid: BoundedVec<u8, ConstU32<64>>,
+    packmsg_cid: BoundedVec<u8, ConstU32<64>>,
+}
+const MAX_NUMBER_PENDING_CIRCUITS_PER_ACCOUNT: u32 = 16;
+type PendingCircuitsType = BoundedVec<StrippedCircuitPackage,ConstU32<MAX_NUMBER_PENDING_CIRCUITS_PER_ACCOUNT>,>;
+
+// https://github.com/scs/substrate-api-client/blob/master/examples/example_get_storage.rs
+// TODO use get Account form passed "api"?(ie DO NOT hardcode Alice)
+fn get_pending_circuits(
+    api: &Api<sp_core::sr25519::Pair, WsRpcClient>,
+) -> PendingCircuitsType {
+    let account = AccountKeyring::Alice.public();
+    // let result: AccountInfo = api
+    //     .get_storage_map("System", "Account", account, None)
+    //     .unwrap()
+    //     .or_else(|| Some(AccountInfo::default()))
+    //     .unwrap();
+    // TODO use _proof?
+    let result: PendingCircuitsType = api
+        .get_storage_map("OcwGarble", "AccountToPendingCircuitsMap", account, None)
+        .unwrap()
+        .unwrap();
+    println!("[+] pending circuits for account = {:?}", result);
+
+    result
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::call_extrinsic;
     use crate::loggers;
+    use crate::{call_submit_config_display_signed, get_api, get_pending_circuits};
+    static INIT: std::sync::Once = std::sync::Once::new();
 
     fn init() {
-        loggers::init_logger();
+        INIT.call_once(|| {
+            loggers::init_logger();
+        });
     }
 
     #[test]
-    fn call_extrinsic_local_ok() {
+    fn call_call_submit_config_display_signed_local_ok() {
         init();
+        let api = get_api("ws://127.0.0.1:9944");
 
         // IMPORTANT this extrinsic requires IPFS!
         // IPFS_PATH=/tmp/ipfs ipfs init -p test
@@ -93,7 +137,16 @@ mod tests {
         // IMPORTANT also requires a running "api_circuits"
         // Seems to be OK with wss eg "wss://polkadot.api.onfinality.io/public-ws"
         // TODO add integration test with SSL
-        let tx_hash = call_extrinsic("ws://127.0.0.1:9944");
+        let tx_hash = call_submit_config_display_signed(&api, false);
         println!("[+] tx_hash: {:02X}", tx_hash);
+    }
+
+    #[test]
+    fn get_pending_circuits_local_ok() {
+        init();
+        let api = get_api("ws://127.0.0.1:9944");
+
+        let pending_circuits = get_pending_circuits(&api);
+        println!("[+] pending_circuits: {:?}", pending_circuits);
     }
 }
